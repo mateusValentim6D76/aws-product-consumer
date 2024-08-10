947640e8499523acb8650ccf9b6cfafc742892bd
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2024-08-10 15:41:23 -0300

    Primeiro commit

---
+++ src/main/java/br/com/mv/cloud/aws/product_consumer/ProductConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mv.cloud.aws.product_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mv/cloud/aws/product_consumer/ProductConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.cloud.aws.product_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
